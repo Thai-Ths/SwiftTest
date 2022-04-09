7a226b706a400234e311bafba920683c91d33436
Thai-Ths <102790509+Thai-Ths@users.noreply.github.com>
2022-04-09 20:57:08 +0700

    Delete swift directory

--- swift/send/router.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from .viewsets import EmailViewSet
-
-app_name = "send"
-
-router = routers.DefaultRouter()
-router.register('mail', EmailViewSet)
--- swift/send/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import SendMail
-
-class EmailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SendMail
-        fields = ['url', 'name', 'subject', 'to', 'body']
